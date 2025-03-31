5db1df0c27a9f732f9e9008b3a55965575122304
Erick Lima <erickcreimberg@gmail.com>
2025-03-30 21:18:11 -0300

    Fazendo a soma de prefixos

--- aula-04/main.c
+++ aula-04/main.c
@@ -4,52 +4,120 @@
 #include <math.h>
 #include <time.h>
 
-int *soma_de_prefixos(int* vetor, int size){
-    int *vetor_b = (int*)malloc(sizeof(int) * size * 2);
-    int *vetor_p = (int*)malloc(sizeof(int) * size * 2);
-    int logSize = (int) log2(size);
+int* soma_de_prefixos(int* A, int n) {
+    int *B = (int*)malloc(sizeof(int) * (2 * n));
+    int *P = (int*)malloc(sizeof(int) * (2 * n));
 
-    #pragma omp parallel shared(vetor, size, logSize)
-    {
-        #pragma omp for
-        for(int i = 0; i < size; i++){
-            vetor_b[i + size] = vetor[i];
-        }   
+    #pragma omp parallel for
+    for (int i = 0; i < n; i++) {
+        B[n + i] = A[i];
+    }
 
-        for(int j = logSize; j >= 0; j--){
-            int exp = (int)exp2(j);
-            int exp_1 = (int)exp2(j + 1);
-         
-            #pragma omp for
-            for(int i = exp; i < exp_1 - 1; i++){
-                vetor_b[i] = vetor_b[2 * i] - vetor_b[2 * i - 1];
-            }
+    int logN = (int)log2(n);
+    for (int j = logN - 1; j >= 0; j--) {
+        int expon2 = 1 << j;
+        int expon2_1   = 1 << (j + 1);
+
+        #pragma omp parallel for
+        for (int i = expon2; i < expon2_1; i++) {
+            B[i] = B[2*i] + B[2*i + 1];
         }
+    }
 
-        vetor_p[1] = vetor_b[1];
+    P[1] = 0;
 
-        for(int j = 1; j < logSize; j++){
-            int exp = (int)exp2(j);
-            int exp_1 = (int)exp2(j + 1);
-         
-            #pragma omp for
-            for(int i = exp; i < exp_1 - 1; i++){
-                if(i % 2 == 0){
-                    vetor_p[i] = vetor_p[(int)i/2] - vetor_b[i + 1];
-                } else {
-                    vetor_p[i] = vetor_p[(int)(i - 1)/2];
-                }
+    for (int j = 0; j < logN; j++) {
+        int expon2 = 1 << j;
+        int expon2_1 = 1 << (j + 1);
+
+        #pragma omp parallel for
+        for (int i = expon2; i < expon2_1; i++) {
+            int parte_esq  = 2 * i;
+            int parte_dir = parte_esq + 1;
+            if (parte_esq < 2 * n) {
+                P[parte_esq] = P[i];
+            }
+            if (parte_dir < 2 * n) {
+                P[parte_dir] = P[i] + B[parte_esq];
             }
         }
     }
 
-    return vetor_p;
+    int *resultado = (int*)malloc(sizeof(int) * n);
+    #pragma omp parallel for
+    for (int i = 0; i < n; i++) {
+        resultado[i] = P[n + i] + A[i];
+    }
+
+    free(B);
+    free(P);
+
+    return resultado;
 }
 
+void prefix_sum(int *A, int i, int j){
+    if(i >= j)
+        return;
+    
+    int meio = (int) (i+j)/2;
 
+    #pragma omp parallel shared(A, i, j, meio)
+    {
+        prefix_sum(A, i, meio);
+        prefix_sum(A, meio + 1, j);
+        
+        #pragma omp for
+        for(int k = meio + 1; k < j; k++){
+            A[k] = A[k] + A[meio];
+        }
+    }
+}
+
+int *prefix_sum_efficient(int *A, int size){    
+    int *P = (int *) malloc(size * sizeof(int));
+    
+    if (size == 1) {
+        P[0] = A[0];
+    }
+    else {
+        int *B = (int *) malloc((size/2) * sizeof(int));
+
+        #pragma omp parallel for
+        for(int i = 0; i < size/2; i++){
+            B[i] = A[2* i] + A[2 * i + 1];
+        }
+
+        int *C = prefix_sum_efficient(B, size/2);
+        free(B);
+
+        P[0] = A[0];
+
+        #pragma omp parallel for
+        for(int i = 1; i < size; i++){
+            if(i % 2 == 1)
+                P[i] = C[i/2];
+            else
+                P[i] = C[(i-1)/2] + A[i];
+        }
+        free(C);
+    }
+    
+    return P;
+}
+
+void print_array(int *A, int size){
+    for(int i = 0; i < size; i++){
+        printf("%d ", A[i]);
+    }
+    printf("\n===========================\n");
+}
 
 int main(){
-
+    int A[4] = {1, 2, 3, 4};
+    int *result = soma_de_prefixos(A, 4);
+    print_array(result, 4);
+    result = prefix_sum_efficient(A, 4);
+    print_array(result, 4);
 
     return 0;
 }
